0f6e4a9313f628f93c3d8163ad5e6d3c8a52b914
Pedro Basilio <phbasilio6903@gmail.com>
2025-02-28 10:03:56 -0300

    Initial commit

---
+++ src/main/java/com/br/api_transactions/ApiTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.br.api_transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/api_transactions/ApiTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.api_transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
